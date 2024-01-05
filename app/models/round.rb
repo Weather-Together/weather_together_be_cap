00cc22577d4d8bd43bd61017edc2d701789fb1d7
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-01-04 17:42:46 -0700

    Add rest of relationships

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,6 @@
 class Round < ApplicationRecord
   belongs_to :game
   has_many :votes
+  has_many :users, through: :votes
 
 end
--- spec/models/user_games_spec.rb
+++ spec/models/user_games_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserGame, type: :model do
   describe "relationships" do
     it { should belong_to(:user) }
     it { should belong_to(:game) }
-    it { should have_many(:rounds).through(:games) }
+    it { should have_many(:rounds).through(:game) }
   end
 
   # describe "validations" do
