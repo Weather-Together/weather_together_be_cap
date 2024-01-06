0ccd4e3648ce70c3a7e3bb1bf795f96c3854998a
Captainlearyo <clayoleary@gmail.com>
2024-01-05 20:30:35 -0600

    add unique email/username and password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_234930) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_06_020210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_04_234930) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.boolean "verified", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
