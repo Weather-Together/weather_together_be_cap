41a288f31b4aadb29a400f93a7a6cc4b80d3babd
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-01-06 12:35:54 -0700

    Fix spec test data, and remove verification validation

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:verified) }
 
   end
 end
