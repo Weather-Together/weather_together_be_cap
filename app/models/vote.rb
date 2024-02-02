409ec47e8a03787c1abd064bdc4b334c94c6239c
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-02-02 12:46:17 -0800

    fix: Update creation of Votes and Rounds

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -19,19 +19,36 @@ class Vote < ApplicationRecord
         new_lon = rand(-180.000...180.000)
       end
       new_score = calculate_score(data)
-      update(status: :processed, score: new_score, weather_stats: data)
+      update(status: :processed, 
+            score: new_score, 
+            target_weather_stats: data,
+            game_id: game.id,
+            location_name: data[:location][:name],
+            region: data[:location][:region],
+            country: data[:location][:country],
+            # lat: data[:location][:lat],
+            # lon: data[:location][:lon],
+            maxtemp_f: data[:weather_data][:maxtemp_f],
+            mintemp_f: data[:weather_data][:mintemp_f],
+            maxwind_mph: data[:weather_data][:maxwind_mph],
+            totalprecip_in: data[:weather_data][:totalprecip_in],
+            avgvis_miles: data[:weather_data][:avgvis_miles],
+            avghumidity: data[:weather_data][:avghumidity],
+            daily_chance_of_rain: data[:weather_data][:daily_chance_of_rain],
+            daily_chance_of_snow: data[:weather_data][:daily_chance_of_snow],
+      )
     end
   end
 
   def calculate_score(weather_data)
-    target = JSON.parse(target_weather_stats, symbolize_names: true)[:weather_data]
-    guess = JSON.parse(weather_data, symbolize_names: true)[:weather_data]
-    high_temp_diff = (target[:maxtemp_f]-guess[:maxtemp_f])**2
-    low_temp_diff = (target[:mintemp_f]-guess[:mintemp_f])**2
-    humidity_diff = (target[:avghumidity]-guess[:avghumidity])**2
-    wind_diff = (target[:maxwind_mph]-guess[:maxwind_mph])**2
-    rain_diff = (target[:daily_chance_of_rain]-guess[:daily_chance_of_rain])**2
-    snow_diff = (target[:daily_chance_of_snow]-guess[:daily_chance_of_snow])**2
+    require 'pry'; binding.pry
+    guess = weather_data[:weather_data]
+    high_temp_diff = (round.maxtemp_f-guess[:maxtemp_f])**2
+    low_temp_diff = (round.mintemp_f-guess[:mintemp_f])**2
+    humidity_diff = (round.avghumidity-guess[:avghumidity])**2
+    wind_diff = (round.maxwind_mph-guess[:maxwind_mph])**2
+    rain_diff = (round.daily_chance_of_rain-guess[:daily_chance_of_rain])**2
+    snow_diff = (round.daily_chance_of_snow-guess[:daily_chance_of_snow])**2
     high_temp_diff + low_temp_diff + humidity_diff + wind_diff + rain_diff + snow_diff
   end
 end
