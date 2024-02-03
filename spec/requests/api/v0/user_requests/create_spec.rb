1f860950573fc4528595284d7d39a12aebe98c57
Edward A Rodriguez <erodriguez23@turing.edu>
2024-02-03 14:13:19 -0600

    Update create_spec.rb

--- spec/requests/api/v0/user_requests/create_spec.rb
+++ spec/requests/api/v0/user_requests/create_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "User Recent Rounds" do
     test_data
     load_location_coordinates
     new_user = {
-      username: "mbkuhl",
-      email: "kuhlmmb@gmail.com",
-      password: "ThisIsAGoodPassword1!",
-      password_confirmation: "ThisIsAGoodPassword1!"
-    }
+                username: "mbkuhl",
+                email: "kuhlmmb@gmail.com",
+                password: "ThisIsAGoodPassword1!",
+                password_confirmation: "ThisIsAGoodPassword1!"
+               }
 
     post "/api/v0/users", params: new_user.to_json, headers: {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
     expect(response).to be_successful
