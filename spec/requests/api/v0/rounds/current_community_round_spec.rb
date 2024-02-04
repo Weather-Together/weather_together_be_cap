f8f982401b52fa81e977accdb042f3f80598662a
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-02-03 23:16:41 -0700

    Change community round test to properly attempt to change game logic over to competitive round naming

--- spec/requests/api/v0/rounds/current_community_round_spec.rb
+++ spec/requests/api/v0/rounds/current_community_round_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Current Community Round", :vcr do
+RSpec.describe "Current Competitive Round", :vcr do
   
-  it "get request for /rounds/current_community_rounds" do
+  it "get request for /rounds/current_competitive_round" do
     load_test_data
     get "/api/v0/rounds/current_community_round"
 
     expect(response).to be_successful
     
-    json_response = JSON.parse(response.body)
-    expect(json_response['data'].length).to eq(3)
+    round = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(round[:type]).to eq("round")
+    expect(round[:attributes][:status]).to eq("open")
+    expect(round[:attributes][:close_date]).to eq((Date.today+1).strftime('%F'))
   end
 end
