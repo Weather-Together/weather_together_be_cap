27a1ad27e7e248179920a9358566d9b17895b18f
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-02-04 02:33:22 -0700

    Implement invites after game has been created with testing

--- app/controllers/api/v0/users/games_controller.rb
+++ app/controllers/api/v0/users/games_controller.rb
@@ -89,6 +89,25 @@ class Api::V0::Users::GamesController < ApplicationController
     end
   end
 
+  def invite
+    admin_user = User.find(params[:user_id])
+    game = Game.find(params[:game_id])
+    missing_accounts = []
+    params[:invitees].each do |email|
+      invitee = User.find_by(email: email)
+      if invitee
+        UserGame.create(user_id: invitee.id, game_id: game.id)
+      else
+        missing_accounts << email
+        InviteMailer.with(invitee: email, admin: admin_user.username).invite_email.deliver_now
+      end
+    end
+    unless missing_accounts.empty?
+      AdminNotifierMailer.with(admin: admin_user.email, game_name: game.name, missing_accounts: missing_accounts ).notify_email.deliver_now
+    end
+    render json: { data: { message: "Successfully Sent Invitation"}}, status: 201
+  end
+
   private
 
   def game_params
