2fd7f4826697d80b8e76c2e649631eed7382c1df
Blaine Kennedy <blainekennedy@gmail.com>
2024-02-08 21:34:47 -0700

    fix the daily vote so it cant be over water

--- app/controllers/api/v0/users/rounds_controller.rb
+++ app/controllers/api/v0/users/rounds_controller.rb
@@ -20,12 +20,19 @@ class Api::V0::Users::RoundsController < ApplicationController
 
   def daily_vote
     round = Game.current_daily_round
-    vote = Vote.create!(lat: params[:lat],
-      lon: params[:lon],
+    original_lat = params[:lat]
+    original_lon = params[:lon]
+    vote = Vote.create!(lat: original_lat,
+      lon: original_lon,
       user_id: params[:user_id],
       round_id: round.id)
     vote.process
-    render json: VoteSerializer.new(vote), status: 201
+    if (vote.lat.to_f - original_lat.to_f).abs >=0.5 || (vote.lon.to_f - original_lon.to_f).abs >=2.0
+        vote.destroy
+        render json: { error: 'Your vote is over water. Please pick a new location.' }, status: 400
+    else
+      render json: VoteSerializer.new(vote), status: 201
+    end
   end
 
   private
--- spec/requests/api/v0/users/rounds/daily_vote_spec.rb
+++ spec/requests/api/v0/users/rounds/daily_vote_spec.rb
@@ -31,4 +31,34 @@ describe "Daily game vote" do
 
     vote = Vote.find_by(user_id: @user3.id, round_id: @round12)
   end
+
+  it "create vote" do
+    VCR.turn_off!
+    WebMock.allow_net_connect!
+
+    test_data
+    load_location_coordinates
+
+    new_vote = {
+      "lat": "0.00}",
+      "lon": "30.00",
+    }
+
+    vote = Vote.find_by(user_id: @user3.id, round_id: @round12)
+    vote.destroy
+
+    post "/api/v0/users/#{@user3.id}/rounds/current_daily_round/vote", params: new_vote.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+    VCR.turn_on!
+    WebMock.disable_net_connect!
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+
+    vote1 = JSON.parse(response.body)
+
+    expect(vote1["error"]).to eq("Your vote is over water. Please pick a new location.")
+    
+
+    vote = Vote.find_by(user_id: @user3.id, round_id: @round12)
+  end
+  
 end
