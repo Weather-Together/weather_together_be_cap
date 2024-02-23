7780f3ed109045ce8cf40343d267f481cbce1d1a
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-02-23 14:14:11 -0700

    feat: Add User Show happy path endpoint and testing

--- spec/requests/api/v0/user_requests/show_spec.rb
+++ spec/requests/api/v0/user_requests/show_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "User Show Profile Endpoint" do
       load_test_data
       
       user1 = @user1
-      
+
       get "/api/v0/users/#{user1.id}", headers: {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
 
       expect(response).to be_successful
@@ -17,8 +17,8 @@ RSpec.describe "User Show Profile Endpoint" do
       expect(response_data[:type]).to eq("user")
       expect(response_data[:id]).to be_a(String)
 
-      expect(response_data[:attributes][:email]).to eq("kuhlmmb@gmail.com")
-      expect(response_data[:attributes][:username]).to be_a("mbkuhl")
+      expect(response_data[:attributes][:email]).to eq("user1@gmail.com")
+      expect(response_data[:attributes][:username]).to eq("username1")
     end
   end
 end
