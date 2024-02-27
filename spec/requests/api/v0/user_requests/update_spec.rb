ba2415c048602a2ee9e1cfd66869efadc06c934a
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-02-27 09:32:26 -0700

    test: Add some more sad path testing to User Update spec

--- spec/requests/api/v0/user_requests/update_spec.rb
+++ spec/requests/api/v0/user_requests/update_spec.rb
@@ -41,5 +41,39 @@ RSpec.describe "User Update Profile Endpoint" do
 
       expect(response_data[:detail]).to eq("Couldn't find User with 'id'=#{user1_id}")
     end
+
+    it "returns an error if attribute being updated is left blank", :vcr do
+      test_data
+
+      user1_id = 11
+
+      expect(User.exists?(user1_id)).to be_falsey
+
+      patch "/api/v0/users/#{user1_id}?username=" #, params: { username: "username0" }, headers: {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      response_data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+      expect(response_data[:detail]).to eq("Username cannot be blank")
+    end
+
+    it "returns an error if username already exists", :vcr do
+      test_data
+
+      user1 = @user1
+
+      expect(user1.username).to eq("username1")
+
+      patch "/api/v0/users/#{user1_id}?username=username1" #, params: { username: "username0" }, headers: {"CONTENT_TYPE" => "application/json", "ACCEPT" => "application/json"}
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+
+      response_data = JSON.parse(response.body, symbolize_names: true)[:errors][0]
+
+      expect(response_data[:detail]).to eq("Username already exists")
+    end
   end
 end
