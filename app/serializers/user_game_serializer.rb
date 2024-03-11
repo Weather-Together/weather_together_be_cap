6b3261e5840b3fc3d5d3d3c7c939fcc5dd120675
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-03-11 11:26:26 -0600

    Add player count and round status to user games

--- app/serializers/user_game_serializer.rb
+++ app/serializers/user_game_serializer.rb
@@ -8,4 +8,12 @@ class UserGameSerializer
   attribute :game_name do |object|
     object.game&.name
   end
+
+  attribute :player_count do |object|
+    object.game&.users.count
+  end
+
+  attribute :rounds do |object|
+    "#{object.game&.rounds.count}/#{object.game&.length_in_days}"
+  end
 end
