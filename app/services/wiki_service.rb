f2f1b73dd94135d81c5410160e39412ba2777d32
Sam Tran <samuelttran@gmail.com>
2024-08-23 18:53:11 -0600

    Write #get_wikipage(titles), for wiki api page call

--- app/services/wiki_service.rb
+++ app/services/wiki_service.rb
@@ -1,5 +1,10 @@
 class WikiService
   def conn
-    Faraday.new(url: "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&inprop=url&titles=")
+    Faraday.new(url: "https://en.wikipedia.org/w/api.php")
+  end
+
+  def get_wikipage(titles)
+    response = conn.get("?action=query&format=json&prop=info&inprop=url&titles=#{titles}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/wiki_service_spec.rb
+++ spec/services/wiki_service_spec.rb
@@ -7,5 +7,40 @@ RSpec.describe "Wiki API call", :vcr do
     expect(ws.conn).to be_a(Faraday::Connection)
   end
 
+  it "#get_wikipage(titles), gets hash both when page exists and does not exists", :vcr do
+    ws = WikiService.new
+    data = ws.get_wikipage("Denver")
+    data_nopage = ws.get_wikipage("18372861jsja")
+
+    expect(data).to be_a(Hash)
+    expect(data[:query]).to be_a(Hash)
+    expect(data_nopage).to be_a(Hash)
+    expect(data_nopage[:query]).to be_a(Hash)
+  end
+
+  xit "#get_link(location), returns a wiki link", :vcr do
+    load_location_coordinates
+
+    location = GeocodingService.new.get_location(lat3, @lon3)
+    ws = WikiService.new
+    data = ws.get_link(location)
+
+    expect(data).to be_a(Hash)
+    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Denver")
+    expect(data[:specificity]).to eq("locality")
+  end
+
+  xit "#returns_article(location_info), determines article number", :vcr do
+    load_location_coordinates
+
+    location = GeocodingService.new.get_location(lat3, @lon3)
+    ws = WikiService.new
+    data = ws.get_link(location)
+
+    expect(data).to be_a(Hash)
+    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Denver")
+    expect(data[:specificity]).to eq("locality")
+  end
+
 
 end
