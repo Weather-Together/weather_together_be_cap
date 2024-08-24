9609b5a39a6173d2e2f66c1b9ceb3c113220cdd5
Sam Tran <samuelttran@gmail.com>
2024-08-24 08:34:40 -0600

    Add #valid_article? and tests for #get_link

--- app/services/wiki_service.rb
+++ app/services/wiki_service.rb
@@ -3,7 +3,7 @@ class WikiService
     Faraday.new(url: "https://en.wikipedia.org/w/api.php")
   end
 
-  def get_wikipage(titles)
+  def parse_wikipage(titles)
     response = conn.get("?action=query&format=json&prop=info&inprop=url&titles=#{titles}")
     pages = JSON.parse(response.body, symbolize_names: true)[:query][:pages]
     articlenum = pages.keys.first
@@ -13,5 +13,9 @@ class WikiService
       articlenum: articlenum.to_s.to_i,
       url: url,
     }
+  end
+
+  def valid_article?(wikipage_data)
+    wikipage_data[:articlenum] > 0
   end
 end
--- spec/services/wiki_service_spec.rb
+++ spec/services/wiki_service_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe "Wiki API call", :vcr do
     expect(ws.conn).to be_a(Faraday::Connection)
   end
 
-  it "#get_wikipage(titles), gets wiki article and url both when page exists and does not exists", :vcr do
+  it "#parse_wikipage(titles), gets wiki article and url both when page exists and does not exists", :vcr do
     ws = WikiService.new
-    data = ws.get_wikipage("Denver")
-    data_nopage = ws.get_wikipage("Nnsense112233")
+    data = ws.parse_wikipage("Denver")
+    data_nopage = ws.parse_wikipage("Nnsense112233")
 
     expect(data).to be_a(Hash)
     expect(data[:articlenum]).to eq(8522)
@@ -19,29 +19,75 @@ RSpec.describe "Wiki API call", :vcr do
     expect(data_nopage[:url]).to eq("https://en.wikipedia.org/wiki/Nnsense112233")
   end
 
-  xit "#get_link(location), returns a wiki link", :vcr do
+  it "#valid_article?(wikipage_data), returns true if articlenum > 0", :vcr do
+    ws = WikiService.new
+    data = ws.parse_wikipage("Denver")
+    data_nopage = ws.parse_wikipage("Nnsense112233")
+
+    expect(ws.valid_article?(data)).to be(true)
+    expect(ws.valid_article?(data_nopage)).to be(false)
+  end
+
+  it "#get_link(location), returns a wiki link from location hash, with increasing specificity, locality", :vcr do
     load_location_coordinates
 
-    location = GeocodingService.new.get_location(lat3, @lon3)
+    location_locality = GeocodingService.new.get_location(lat3, @lon3)
     ws = WikiService.new
-    data = ws.get_link(location)
+    data = ws.get_link(location_locality)
 
     expect(data).to be_a(Hash)
     expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Denver")
     expect(data[:specificity]).to eq("locality")
   end
 
-  xit "#returns_article(location_info), determines article number", :vcr do
-    load_location_coordinates
-
-    location = GeocodingService.new.get_location(lat3, @lon3)
+  it "#get_link(location), returns a wiki link from location hash, with increasing specificity, state", :vcr do
+    location_state =  {
+      Locationlat: "39.74",
+      Locationlon: "-104.99",
+      locality: nil,
+      state: "Colorado",
+      region: "Midwest",
+      country: "United States"
+    }
     ws = WikiService.new
-    data = ws.get_link(location)
+    data = ws.get_link(location_state)
 
     expect(data).to be_a(Hash)
-    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Denver")
-    expect(data[:specificity]).to eq("locality")
+    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Colorado")
+    expect(data[:specificity]).to eq("state")
   end
 
+  it "#get_link(location), returns a wiki link from location hash, with increasing specificity, region", :vcr do
+    location_state =  {
+      Locationlat: "39.74",
+      Locationlon: "-104.99",
+      locality: nil,
+      state: nil,
+      region: "Midwest",
+      country: "United States"
+    }
+    ws = WikiService.new
+    data = ws.get_link(location_state)
 
+    expect(data).to be_a(Hash)
+    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/Midwest")
+    expect(data[:specificity]).to eq("region")
+  end
+
+  it "#get_link(location), returns a wiki link from location hash, with increasing specificity, country", :vcr do
+    location_state =  {
+      Locationlat: "39.74",
+      Locationlon: "-104.99",
+      locality: nil,
+      state: nil,
+      region: nil,
+      country: "United States"
+    }
+    ws = WikiService.new
+    data = ws.get_link(location_state)
+
+    expect(data).to be_a(Hash)
+    expect(data[:url]).to eq("https://en.wikipedia.org/wiki/United_States")
+    expect(data[:specificity]).to eq("region")
+  end
 end
